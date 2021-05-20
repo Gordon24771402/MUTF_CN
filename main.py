50ab41ffc12fee5b49c2bd999441b134f4f4fdeb
GK24771402 <63176281+Gordon24771402@users.noreply.github.com>
2021-05-20 18:29:17 +0800

    1) make the program less resource-consuming

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from capture import captureTopic
 from store import storeTopic
 from datetime import datetime
+import time
 
 print("DataBase is Ready to Run!\n")
 
 while True:
-    now = datetime.now().strftime("%H:%M:%S")
-    if now in ["09:35:00", "10:35:00", "11:35:00", "13:35:00", "14:35:00", "15:35:00"]:
+    now = datetime.now().strftime("%H:%M")
+    if now in ["09:35", "10:35", "11:35", "13:35", "14:35", "15:35"]:
         dataTopic = captureTopic()
         print("{} dataTopic is Captured".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
         storeTopic(dataTopic)
         print("{} dataTopic is Stored\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        time.sleep(60)
+    else:
+        time.sleep(60)
 
 # pyinstaller -F --add-binary "C:\Users\97262\OneDrive\Pycharm\MUTF_CN\chromedriver.exe";"." --clean main.py
