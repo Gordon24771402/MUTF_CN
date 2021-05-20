5a0b87a7943156fbf515684b9665e5b48c2c1311
GK24771402 <63176281+Gordon24771402@users.noreply.github.com>
2021-05-20 18:51:11 +0800

    1) check trading date or not

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from capture import captureTopic
 from store import storeTopic
 from datetime import datetime
-import time
+from requests import get
+from time import sleep
 
 print("DataBase is Ready to Run!\n")
 
 while True:
-    now = datetime.now().strftime("%H:%M")
-    if now in ["09:35", "10:35", "11:35", "13:35", "14:35", "15:35"]:
-        dataTopic = captureTopic()
-        print("{} dataTopic is Captured".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-        storeTopic(dataTopic)
-        print("{} dataTopic is Stored\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-        time.sleep(60)
+    date, time = datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%H:%M")
+    calendar = get("http://fund.eastmoney.com/fundguzhi.html").text
+    if date in calendar:
+        if time in ["09:35", "10:35", "11:35", "13:35", "14:35", "15:35"]:
+            dataTopic = captureTopic()
+            print("{} dataTopic is Captured".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+            storeTopic(dataTopic)
+            print("{} dataTopic is Stored\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+            sleep(60)
+        else:
+            sleep(60)
     else:
-        time.sleep(60)
+        sleep(1800)
 
 # pyinstaller -F --add-binary "C:\Users\97262\OneDrive\Pycharm\MUTF_CN\chromedriver.exe";"." --clean main.py
--- store.py
+++ store.py
@@ -1,9 +1,9 @@
-import os
+from os.path import getsize
 
 
 def storeTopic(data):
     for key in data["content"].keys():
         with open("./Data/ByTopic/{}.csv".format(key), "a") as df:
-            if os.path.getsize("./Data/ByTopic/{}.csv".format(key)) != 0:
+            if getsize("./Data/ByTopic/{}.csv".format(key)) != 0:
                 df.write("\n")
             df.write("{},{},{}".format(data["date"], data["time"], data["content"][key]))
