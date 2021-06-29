1e2c66837f8466411ad54f291733db46caecb005
GK24771402 <63176281+Gordon24771402@users.noreply.github.com>
2021-06-29 11:02:13 +0800

    1) def benchmark_ratio(fCode, cPeriod=0, sDate='', eDate=''):

--- DataBase.py
+++ DataBase.py
@@ -39,6 +39,18 @@ def benchmark_ratio(fCode, cPeriod=0, sDate='', eDate=''):
     # 排除非法参数： 1) cPeriod与(sDate或eDate)同时出现 2) 有sDate无eDate 3) 有eDate无sDate
     if (cPeriod and (sDate or eDate)) or (sDate and not eDate) or (eDate and not sDate):
         raise ValueError("Illegal Argument")
+    # 用akshare获取：开放式基金-历史数据
+    fInfo = ak.fund_em_open_fund_info(fund=fCode, indicator="单位净值走势")
+    # 用cPeriod参数：选出历史数据区间
+    if cPeriod:
+        fInfo = fInfo[-cPeriod:].reset_index(drop=True)
+    # 用sDate和eDate参数：选出历史数据区间
+    elif sDate and eDate:
+        sDate, eDate = datetime.strptime(sDate, "%Y-%m-%d").date(), datetime.strptime(eDate, "%Y-%m-%d").date()
+        fInfo = fInfo.loc[(fInfo["净值日期"] >= sDate) & (fInfo["净值日期"] <= eDate)].reset_index(drop=True)
 
 
-print(help(max_drawdown))
+
+# print(benchmark_ratio("005888", cPeriod=180))
+stock_zh_index_daily_df = ak.stock_zh_index_daily(symbol="sh000300")
+print(stock_zh_index_daily_df)
